220ed66092c0f64773b59b3ce06750a4b1d9b261
LuchoDelArco <47138072@est.ort.edu.ar>
2022-04-08 11:35:58 -0300

    Exercises Completed

---
+++ Assets/Ej01.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son iguales o no lo son
+
+public class Ej01 : MonoBehaviour
+{
+	public int numero1;
+	public int numero2;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		if (numero1 == numero2)
+		{
+			Debug.Log("Los números son iguales");
+		}
+		else
+		{
+			Debug.Log("Los números son diferentes");
+		}
+	}
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Ej02.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Crear las variables necesarias para ingresar por Inspector el precio de 3 productos y un monto de dinero disponible.
+//Comunicar si la suma de los mismos supera o no el monto de dinero disponible. Luego indicar cuánto dinero sobra o falta.
+
+public class Ej02 : MonoBehaviour
+{
+
+	public float producto1;
+	public float producto2;
+	public float producto3;
+	float sumaProductos;
+
+	public float montoDisponible;
+	float faltaDinero;
+	float sobraDinero;
+    
+
+    void Start()
+    {
+		sumaProductos = producto1 + producto2 + producto3;
+		if (montoDisponible < sumaProductos)
+		{
+			faltaDinero = sumaProductos - montoDisponible;
+			Debug.Log("La suma de los productos superan al monto disponible. " + "Se necesitan $" + faltaDinero + "más");
+		}
+		else if (montoDisponible > sumaProductos)
+		{
+			sobraDinero = montoDisponible - sumaProductos;
+			Debug.Log("La suma de los productos no supera al monto disponible. " + "Sobran $" + sobraDinero);
+		}
+		else
+		{
+			Debug.Log("La suma de los productos es igual al monto. " + "No hay resto");
+		}
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
